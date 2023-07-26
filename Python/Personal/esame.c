d3af6af0768a66dbf4ab777fdb5db786133064b7
fadhla <fgfug898@gmail.com>
2023-07-26 18:38:02 +0200

    fixed esame

--- Python/Personal/esame.c
+++ Python/Personal/esame.c
@@ -4,10 +4,18 @@
 int main(int argc, char * arvg[]){
     pid_t pid1 = fork();
     pid_t pid2 = fork();
+    if (pid1 != 0)
+    {
+        pid1 = 1;
+    }
+    if (pid2 != 0)
+    {
+        pid2 = 1;
+    }
     if (pid1 == 1){
-        printf("a\n");
+        printf("a ");
     }else if (pid2 == 1) {
-        printf("b\n");
+        printf("b ");
     }else{
         printf("c\n");
     }
