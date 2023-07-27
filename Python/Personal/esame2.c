f0daf3dd1ba0dababd2b543a4b0dd7f9352cf161
fadhla <fgfug898@gmail.com>
2023-07-27 19:47:34 +0200

    Formulae for Babu Tom

--- Python/Personal/esame2.c
+++ Python/Personal/esame2.c
@@ -1,39 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
+#include <math.h>
 
-pthread_mutex_t lockx = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t locky = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t locki = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+float i = 0;
+int k = 0;
+float *n;
+int flag = 1;
 
-int x = 1000000;
-int y = 1000000;
-
-void * f (void*arg)
+void * f (void*argc)
 {
-    pthread_mutex_lock(&lockx);
-    x++;
-    pthread_mutex_unlock(&lockx);
-    pthread_mutex_lock(&locky);
-    y += x;
-    pthread_mutex_unlock(&locky);
-    return NULL;
-}
+    float z = 0;
+    while(i < 1000000){
+        pthread_mutex_lock(&locki);
+        pthread_cond_wait(&locki,&cond);
+    
+        z = 8*i + (4*i + 1)/7;
+        //printf("trying i = %d, n = 0\n",(int)i);
+        if ((int)z == z){
+            *(n+k) = z;
+            //printf("n = %f\n",*(n+k));
+            k++;
+        }
+        i = i + 1;
 
+        pthread_mutex_unlock(&locki);
+        pthread_cond_signal(&cond);
+    }
+    return n;
+}
 
 int main(int argc, char * argv[])
 {
+    n = (float *)malloc(1000000 * sizeof(float));
     pthread_t t1;
     pthread_t t2;
     pthread_t t3;
     pthread_t t4;
+    pthread_t t5;
+    pthread_t t6;
     pthread_create(&t1,NULL,f,NULL);
     pthread_create(&t2,NULL,f,NULL);
     pthread_create(&t3,NULL,f,NULL);
     pthread_create(&t4,NULL,f,NULL);
+    pthread_create(&t5,NULL,f,NULL);
+    pthread_create(&t6,NULL,f,NULL);
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
     pthread_join(t3,NULL);
     pthread_join(t4,NULL);
-    printf("x = %d, y = %d\n",x,y);
+    
+    for (int i = 0; i < k; i++){
+        printf("i = %f\n",n[i]);
+    }
+    free(n);
     return 0;
 }
