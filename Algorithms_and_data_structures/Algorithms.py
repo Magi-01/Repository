7251ad19eb22a8b59ce6cc284a5b7e2ac6b9c5b3
Magi-01 <fgfug898@gmail.com>
2026-08-23 23:48:03 +0200

    continued DA

--- Algorithms_and_data_structures/Algorithms.py
+++ Algorithms_and_data_structures/Algorithms.py
@@ -39,8 +39,29 @@ class Utility():
 
         return i
 
-    def heapify(self, heap: list) -> list:
-        return heap
+    def heapify(self, array: list, n: int, i: int) -> list:
+        largest = i
+        l = 2*i + 1
+        r = 2*i + 2
+
+        if l < n and array[l] > array[largest]:
+            largest = l
+
+        if r < n and array[r] > array[largest]:
+            largest = i
+
+        if largest != i:
+            array[i], array[largest] = array[largest], array[i]
+
+        self.heapify(array, len(array), largest)
+        return array
+
+    def max_heap(self, array: list) -> list:
+        # Used in Graph algorithms Dijkistra, Prim and Kruksal
+        for i in range(len(array)//2 - 1, -1, -1):
+            self.heapify(array, len(array), i)
+
+        return array
 
     def collision_check(self, val):
         pass
@@ -53,16 +74,15 @@ class Sorting(Utility):
     def __init__(self):
         pass
 
-    def bubble_sort(self, array: list) -> list:
+    def bubble_sort(self, array: list):
         n = len(array)
 
         for i in range(n):
             for j in range(i,n-1):
                 if array[j] > array[j+1]:
                     array[j], array[j+1] = array[j+1], array[j]
-        return array
 
-    def insertion_sort(self, array: list) -> list:
+    def insertion_sort(self, array: list):
         n = len(array)
         i, j = 0, 1
 
@@ -79,17 +99,13 @@ class Sorting(Utility):
 
             i += 1
 
-        return array
-
-    def selection_sort(self, array: list) -> list:
+    def selection_sort(self, array: list):
         n = len(array)
 
         for i in range(n):
             for j in range(i):
                 if array[i] < array[j]:
                     array[j], array[i] = array[i], array[j]
-
-        return array
 
     def merge_sort(self, array: list) -> list:
         n = len(array)
@@ -115,40 +131,111 @@ class Sorting(Utility):
         pi = self.pivot(left, right, array)
         self.quick_sort(left, pi-1, array)
         self.quick_sort(pi+1, right, array)
+
         return array
 
-    def max_heap(self, array: list) -> list:
+    def heap_sort(self, array: list) -> list:
+        n = len(array)
+
+        for i in range(n//2-1, -1, -1):
+            self.heapify(array, len(array), i)
+        for i in range(n-1, 0, -1):
+            array[0], array[i] = array[i], array[0]
+            self.heapify(array, i, 0)
         return array
 
-    def min_heap(self, array: list) -> list:
-        return array
+    def counting_sort(self, array: list, exp1 = 1, radix = False) -> list:
+        n = len(array)
+        ans = [0]*n
+        
+        if radix:
+            bucket = [0]*(10)
 
-    def counting_sort(self, array: list) -> list:
-        return array
+            for i in range(n):
+                idx = array[i] // exp1
+                bucket[idx%10] += 1
+
+            for i in range(1, 10):
+                bucket[i] += bucket[i-1]
+
+            i = n - 1
+
+            while i >= 0:
+                idx = array[i] // exp1
+                ans[bucket[idx%10] - 1] = array[i]
+                bucket[idx%10] -= 1
+                i -= 1
+
+        else:
+            bucket = [0]*(max(array) + 1)
+
+            for i in range(n):
+                bucket[array[i]] += 1
+
+            for i in range(1, max(array) + 1):
+                bucket[i] += bucket[i-1]
+
+            for i in range(n - 1, -1, -1):
+                temp = array[i]
+                ans[bucket[temp] - 1] = temp
+                bucket[temp] -= 1
+
+        return ans
 
     def radix_sort(self, array: list) -> list:
+        large = max(array)
+        exp = 1
+        while large / exp >= 1:
+            self.counting_sort(array, exp, radix=True)
+            exp *= 10
         return array
 
-    def bucket_sort(self, array: list) -> list:
-        return array
+    def bucket_sort(self, array: list):
+        # Space is O(n + k)
+        buckets = [[]*len(array)]
+
+        for num in array:
+            buckets[int(float(len(array)*num))].append(num)
+
+        for bucket in buckets:
+            # Has worst case of O(n^2) if all elements fall within one bucket
+            # in which case it it better to use divide and conquer methods
+
+            # Has best case of O(n + k) where each bucket is equally 
+            # distributed and, as such insertion sort runs on an array of size 
+            # bucket
+            self.insertion_sort(bucket)
+
+        index = 0
+        for bucket in buckets:
+            for num in bucket:
+                array[index] = num
+                index += 1
 
 #-----------------------------------------
 # DATA STORAGE TYPES
 #-----------------------------------------
 
 class LinkedList(Utility):
-    def __init__(self):
-        pass
-
-    def __iter__(self):
-        pass
-
-    def __next__(self):
-        pass
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
 class HashTable(Utility):
-    def __init__(self):
-        pass
+    """
+    Choose between various methods being:
+    - chaining: Linked List
+    - chaining: python List (dynamic arrays)
+    - chaining: Self Balancing BST (AVL)
+    - chaining: Self Balancing BST (Red-Black)
+    - open addressing: Linear Probing
+    - open addressing: Quadratic Probing
+    - open addressing: Double Hashing
+
+    """
+    def __init__(self, val, method = "chaining: Linked List"):
+        self.val = val
+        self.position = self.hashing_function(val, method)
 
     def __iter__(self):
         pass
@@ -156,8 +243,17 @@ class HashTable(Utility):
     def __next__(self):
         pass
 
-    def hashing_function(self, value):
-        pass
+    def hashing_function(self, val, method):
+        if method == "chaining: Linked List":
+            bucket = val%10
+            lst = {}
+            lst[bucket] = LinkedList(val)
+            while lst[bucket].next != None and lst[bucket].next.next != None:
+                lst[bucket].val = lst[bucket].next
+            lst[bucket].next = None # TO BE CONTINUED
+            self.list = lst
+            
+        return val%10
 
 
 class Tree(Utility):
@@ -168,6 +264,11 @@ class Tree(Utility):
         pass
 
     def __next__(self):
+        pass
+
+# Call either max-heap or heapsort
+class Heap(Utility):
+    def __init__(self):
         pass
 
 #-----------------------------------------
